98f270676d1a2bef22a6425677d65005e85f9cc6
kenken <ken2taro.yoshikawa@gmail.com>
2022-07-12 18:20:48 +0900

    fix kf.py

--- kf.py
+++ kf.py
@@ -4,9 +4,11 @@ import os
 import numpy as np
 import torch
 from torchvision.ops.boxes import box_area
+from munkres import Munkres
 
 from tracking.track_3d import Track_3D
 from tracking.kalman_fileter_3d import KalmanBoxTracker
+from detection.detection import Detection_3D_only
 
 
 parser = argparse.ArgumentParser()
@@ -46,10 +48,14 @@ def box_iou(boxes1, boxes2):
 
 
 def main(args):
+    max_age = 25
+    min_hits = 3
     image_filenames, seq_dets_3D = _load_3d(args.video_id, args.data_dir, args.image_dir)
     last_dets_3D_camera = None
     last_dets_3Dto2D_image = None
     trackers = []
+    track_id_3d = 0
+    h = Munkres()
     for frame, _ in enumerate(image_filenames):
         dets_3D_camera = seq_dets_3D[seq_dets_3D[:, 0] == frame, 7:14]  # 3D bounding box(h,w,l,x,y,z,theta)
         # ori_array = seq_dets_3D[seq_dets_3D[:, 0] == frame, -1].reshape((-1, 1))
@@ -59,12 +65,56 @@ def main(args):
         if frame % 2 == 1:
             continue
         if last_dets_3D_camera is None and last_dets_3Dto2D_image is None:
+            for det_3D in dets_3D_camera:
+                detection_3d = Detection_3D_only(det_3D, additional_info=None)
+                kf_3d = KalmanBoxTracker(detection_3d.bbox)
+                pose = np.concatenate(kf_3d.kf.x[:7], axis=0)
+                trackers.append(
+                    Track_3D(
+                        pose=pose,
+                        kf_3d=kf_3d,
+                        track_id_3d=track_id_3d,
+                        n_init=min_hits,
+                        max_age=max_age,
+                        additional_info=None,
+                    )
+                )
+                track_id_3d += 2
             last_dets_3D_camera = dets_3D_camera
             last_dets_3Dto2D_image = dets_3Dto2D_image
             continue
 
         ious = box_iou(last_dets_3Dto2D_image, dets_3Dto2D_image)
-        ious_map = ious > args.iou_thresh
+        if ious.shape[0] > ious.shape[1]:
+            matching = h.compute(ious.T)
+            matching = [(j, i) for i, j in matching]
+        else:
+            matching = h.compute(ious)
+
+        new_trackers = []
+        for matched in matching:
+            last_idx, idx = matched
+            if ious[last_idx, idx] <= args.iou_thresh:
+                # delete unused trackier
+                # create new tracker
+                detection_3d = Detection_3D_only(dets_3D_camera[idx], additional_info=None)
+                kf_3d = KalmanBoxTracker(detection_3d.bbox)
+                pose = np.concatenate(kf_3d.kf.x[:7], axis=0)
+                new_trackers.append(
+                    Track_3D(
+                        pose=pose,
+                        kf_3d=kf_3d,
+                        track_id_3d=track_id_3d,
+                        n_init=min_hits,
+                        max_age=max_age,
+                        additional_info=None
+                    )
+                )
+                track_id_3d += 2
+                continue
+            detection_3d = Detection_3D_only(dets_3D_camera[idx],  additional_info=None)
+            trackers[last_idx].update_3d(detection_3d)
+            new_trackers.append(trackers[last_idx])
 
         last_dets_3D_camera = dets_3D_camera
         last_dets_3Dto2D_image = dets_3Dto2D_image
